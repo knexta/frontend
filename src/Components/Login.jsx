f225d3a85b4e98d5a41ba7343440290cc398474c
knexta <knexta>
2022-11-22 12:20:10 +0530

    pending login

---
+++ src/Components/Login.jsx
@@ -0,0 +1,104 @@
+import axios from "axios";
+import { useFormik } from "formik";
+import React from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { config } from "../config";
+
+function Login() {
+  let navigate = useNavigate();
+  const formik = useFormik({
+    initialValues: {
+      email: "",
+      password: "",
+    },
+    validate: (values) => {
+      let errors = {};
+      if (!values.email) {
+        errors.email = "Please enter email";
+      }
+      if (!values.password) {
+        errors.password = "Please enter password";
+      }
+      return errors;
+    },
+    onSubmit: async (values) => {
+      try {
+        const res = await axios.put(`${config.api}/api/users/login`, values);
+        alert(res.data.message);
+        console.log(res.data.user);
+
+        localStorage.setItem("email", values.email);
+        localStorage.setItem("react_app_token", res.data.token);
+        localStorage.setItem("userid", res.data.user._id);
+        navigate(`/dashboard/${localStorage.getItem("userid")}`);
+
+        // localStorage.setItem("react_app_token",res.data.token)
+      } catch (error) {
+        console.log(error);
+      }
+    },
+  });
+
+  return (
+    <div>
+      <h2 className="text-center mt-5">Authentication</h2>
+      <div className="container">
+        <div className="row m-5 no-gutters shadow-lg">
+          <div className="col-md-6 d-none d-md-block">
+            <img
+              src="https://securityintelligence.com/wp-content/uploads/2018/10/si-advanced-authentication-feature.jpg"
+              className="img-fluid"
+              alt="image"
+              style={{ minHeight: "100%" }}
+            />
+          </div>
+          <div className="col-md-6 bg-white p-5">
+            <h3 className="pb-3">Login Form</h3>
+            <div className="form-style">
+              <form onSubmit={formik.handleSubmit}>
+                <div className="form-group pb-3">
+                  <input
+                    type="text"
+                    placeholder="email"
+                    className="form-control"
+                    id="email"
+                    aria-describedby="emailHelp"
+                    name="email"
+                    value={formik.values.email}
+                    onChange={formik.handleChange}
+                  />
+                </div>
+                <div className="form-group pb-3">
+                  <input
+                    type="password"
+                    placeholder="Password"
+                    className="form-control"
+                    id="exampleInputPassword1"
+                    name="password"
+                    value={formik.values.password}
+                    onChange={formik.handleChange}
+                  />
+                </div>
+
+                <div className="pb-2">
+                  <button
+                    type="submit"
+                    className="btn btn-dark w-100 font-weight-bold mt-2"
+                  >
+                    Submit
+                  </button>
+                </div>
+              </form>
+
+              <div className="pt-4 text-center">
+                Get Members Benefit.<Link to={"/register"}>SignUP</Link>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Login;
---
+++ src/Components/VerifyOTP.jsx
@@ -0,0 +1,103 @@
+import axios from "axios";
+import { useFormik } from "formik";
+import React from "react";
+import { Link, useNavigate, useParams } from "react-router-dom";
+import { config } from "../config";
+
+function VerifyOTP() {
+  let navigate = useNavigate();
+  let handleResend = async () => {
+    try {
+      let res = await axios.post(
+        `${config.api}/api/users/resendOtp`,
+        formik.values
+      );
+      console.log(res);
+      alert(res.data.message);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  let params = useParams();
+  let formik = useFormik({
+    initialValues: {
+      otp: "",
+      userId: params.userid,
+      email: localStorage.getItem("email"),
+    },
+    validate: (values) => {
+      let errors = {};
+      if (!values.otp) {
+        errors.otp = "Please enter otp";
+      }
+      return errors;
+    },
+    onSubmit: async (values) => {
+      // console.log(values)
+      try {
+        const res = await axios.post(
+          `${config.api}/api/users/verifyOtp`,
+          values
+        );
+        navigate("/");
+        // console.log(res)
+        alert(res.data.message);
+      } catch (error) {
+        console.log(error);
+      }
+    },
+  });
+
+  return (
+    <>
+      <h2 className="text-center mt-5">Authentication</h2>
+      <div className="container">
+        <div className="row m-5 no-gutters shadow-lg">
+          <div className="col-md-6 bg-white p-5 ">
+            <h3 className="pb-3">Verify OTP</h3>
+            <div className="form-style">
+              <form onSubmit={formik.handleSubmit}>
+                <div className="form-group pb-3">
+                  <input
+                    type="text"
+                    name="userid"
+                    className="form-control"
+                    value={params.userid}
+                    disabled
+                  />
+                  <input
+                    type="text"
+                    placeholder="Enter OTP"
+                    className="form-control"
+                    id="otp"
+                    name="otp"
+                    value={formik.values.otp}
+                    onChange={formik.handleChange}
+                  />
+                </div>
+
+                <div className="pb-2">
+                  <button
+                    type="submit"
+                    className="btn btn-dark w-100 font-weight-bold mt-2"
+                  >
+                    Verify OTP
+                  </button>
+                  <button
+                    className="btn btn-secondary w-100 font-weight-bold mt-2"
+                    type="button"
+                    onClick={() => handleResend()}
+                  >
+                    Resend OTP
+                  </button>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default VerifyOTP;
